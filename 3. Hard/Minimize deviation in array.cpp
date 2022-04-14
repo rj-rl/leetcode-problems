e61b8d7a890820829fa3c52c2dfe5da4283535d2
Marat K <rjrltsk@yandex.ru>
2022-04-14 22:12:22 +0700

    add recursive Regex Matching

--- 3. Hard/Minimize deviation in array.cpp
+++ 3. Hard/Minimize deviation in array.cpp
@@ -1,17 +1,17 @@
 /*
-          https://leetcode.com/problems/minimize-deviation-in-array/
+  https://leetcode.com/problems/minimize-deviation-in-array/
 
-    You are given an array 'nums' of 'n' positive integers.
-    Two types of operations can be performed on any element any number of times:
+  You are given an array 'nums' of 'n' positive integers.
+  Two types of operations can be performed on any element any number of times:
 
-    If the element is even, it can be divided by 2
-      For example, if the array is [1,2,3,4], you can modify the last element and get [1,2,3,2];
-    If the element is odd, it can be multiplied by 2
-      For example, if the array is [1,2,3,4], you can modify the first element and get [2,2,3,4].
+  If the element is even, it can be divided by 2
+    For example, if the array is [1,2,3,4], you can modify the last element and get [1,2,3,2];
+  If the element is odd, it can be multiplied by 2
+    For example, if the array is [1,2,3,4], you can modify the first element and get [2,2,3,4].
 
-    The deviation of the array is the maximum difference between any two elements in the array.
-    Return the minimum deviation the array can have after performing some number of operations
-*/
+  The deviation of the array is the maximum difference between any two elements in the array.
+  Return the minimum deviation the array can have after performing some number of operations
+/**/
 
 #include <iostream>
 #include <vector>
@@ -25,13 +25,13 @@ int minimumDeviation(const vector<int>& nums)
     int min_distance{ 1'000'000'000 };
     int min{ 1'000'000'000 };
     priority_queue<int> queue;
-    
+
     for (auto x : nums) {   // get rid of all odd nums for uniformity
         x = x % 2 ? x * 2 : x;
         queue.push(x);
         min = std::min(min, x);
     }
-    
+
     int x = queue.top();
     while (x % 2 == 0) {
         queue.pop();
@@ -56,6 +56,7 @@ int main()
         }
         cin.clear();
         cin.ignore();
+
         cout << "Minimum deviation after tweaking:\n"
             << minimumDeviation(v) << '\n';
     }
---
+++ 3. Hard/Regular Expression Matching.cpp
@@ -0,0 +1,54 @@
+/*
+  https://leetcode.com/problems/regular-expression-matching/
+  Given an input string 's' and a pattern 'p', implement regular expression
+  matching with support for '.' and '*' where:
+    '.' matches any single character
+    '*' matches zero or more of the preceding element
+  The matching should cover the entire input string (not partial)
+/**/
+
+#include <iostream>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+bool isMatch(string s, string p)
+{
+    // reached the end simultaneously - we have a match!
+    if (s.empty() && p.empty()) return true;
+    // pattern is exhausted but string is not - mismatch
+    if (!s.empty() && p.empty()) return false;
+
+    bool is_first_char_match
+        = !s.empty() && (s.front() == p.front() || p.front() == '.');
+
+    if (p.length() > 1 && p[1] == '*') {
+        return isMatch(s, p.substr(2))  // this means * pattern is not used at all and is discarded
+            || is_first_char_match && isMatch(s.substr(1), p);  // this means * is used >= 1 times
+    }
+    else return is_first_char_match && isMatch(s.substr(1), p.substr(1));
+}
+
+
+//===========================================================================//
+
+int main()
+{
+    while (true) {
+        cout << "Enter the string:\n";
+        string s;
+        cin >> s;
+        cout << "And the regex pattern:\n";
+        string p;
+        cin >> p;
+
+        if (isMatch(move(s), move(p))) {
+            cout << "The pattern matches the string!\n";
+        }
+        else {
+            cout << "Not a match, mate\n";
+        }
+    }
+    return 0;
+}
